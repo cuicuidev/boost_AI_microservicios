7b148f6aab91e3aca25f03365e036589f6942e39
Dmitry Ryzhenkov <dmitryryzhenkov.dev@gmail.com>
2023-12-06 19:50:38 +0100

    done some more progress

---
+++ api/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle as pkl
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+
+class RequestForm(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+class PredictionForm(BaseModel):
+    species: str
+    
+def get_prediction(data: RequestForm):
+    model = load_model()
+    prediction = model.predict([data.sepal_length, data.sepal_width, data.petal_length, data.petal_width])
+    encoder = load_encoder()
+    prediction = encoder.inverse_transform(prediction)
+    return prediction
+
+def load_model():
+    with open('model.pkl', 'rb') as f:
+        model: RandomForestClassifier = pkl.load(f)
+    return model
+
+def load_encoder():
+    with open('encoder.pkl', 'rb') as f:
+        encoder: LabelEncoder = pkl.load(f)
+    return encoder
+
+###############################################################################################################
+
+app = FastAPI()
+
+@app.post("/predict")
+async def predict(data: RequestForm):
+    prediction = get_prediction(data)
+    response = PredictionForm(category=prediction)
+    return response
